5f52346f6d7cac627d345dc65ad42501a0483926
nights <twonights@proton.me>
2024-10-06 01:17:58 +0800

    Add base

---
+++ src/client/java/com/nights/smmusic/StoryModeMusicClient.java
@@ -0,0 +1,10 @@
+package com.nights.smmusic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StoryModeMusicClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
